d2a1896149903dccd164ae7e4630547d5fe676ec
MarijaK02 <karajanova289@gmail.com>
2024-11-06 17:35:23 +0100

    register validation and company models

---
+++ TTS.Domain/Domain/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTS.Domain.Enum;
+using TTS.Domain.Shared;
+
+namespace TTS.Domain.Domain
+{
+    public class Company : BaseEntity
+    {
+        public string? Address { get; set; }
+        public required string Name { get; set; }
+        public required Industry Industry { get; set; }
+        public string? ContactPhone { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ TTS.Domain/Enum/Industry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTS.Domain.Enum
+{
+    public enum Industry
+    {
+        ECommerce,
+        FinTech,
+        HealthTech,
+        EdTech,
+        Gaming,
+        RealEstate,
+        Travel,
+        Logistics,
+        Entertainment,
+        Cybersecurity,
+        ArtificialIntelligence,
+        DataAnalytics,
+        Telecommunications,
+        Blockchain,
+        CloudComputing,
+        Marketing,
+        Manufacturing,
+        Retail,
+        Government,
+        NonProfit
+    }
+}
